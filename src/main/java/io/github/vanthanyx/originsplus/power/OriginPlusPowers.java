1efa750a6cce8eb5bef47e673005f08be173b84c
Vanthanyx <73613589+Vanthanyx@users.noreply.github.com>
2021-04-30 16:22:58 -0600

    Create OriginPlusPowers.java

---
+++ src/main/java/io/github/vanthanyx/originsplus/power/OriginPlusPowers.java
@@ -0,0 +1,12 @@
+package io.github.vanthanyx.originsplus.power;
+
+import io.github.apace100.origins.power.Power;
+import io.github.apace100.origins.power.PowerType;
+import io.github.apace100.origins.power.PowerTypeReference;
+import net.minecraft.util.Identifier;
+
+public class OriginsPlusPowers {
+  
+  
+  
+}
